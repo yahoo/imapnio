31aebb35dd766e269ec08277a9ca245839fdb54b
Jui Wang <juiwang@yahoo-inc.com>
2019-10-13 21:48:51 -0700

    Support MAILBOXID in ExtensionMailboxInfo.

---
+++ core/src/main/java/com/yahoo/imapnio/async/data/ExtensionMailboxInfo.java
@@ -0,0 +1,66 @@
+package com.yahoo.imapnio.async.data;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.sun.mail.iap.ParsingException;
+import com.sun.mail.imap.protocol.IMAPResponse;
+import com.sun.mail.imap.protocol.MailboxInfo;
+
+/**
+ * This class provides the mailbox information and extension items.
+ */
+public class ExtensionMailboxInfo extends MailboxInfo {
+
+    /** Literal for MAILBOXID. */
+    private static final String MAILBOX_ID = "MAILBOXID";
+
+    /** Variable to store mailbox Id. */
+    private Integer mailboxId;
+
+    /**
+     * Initializes an instance of @{code ExtensionMailboxInfo} from the server responses for the select or examine command.
+     *
+     * @param resps response array from server
+     * @throws ParsingException for errors parsing the responses
+     */
+    public ExtensionMailboxInfo(@Nonnull final IMAPResponse[] resps) throws ParsingException {
+        super(resps);
+        for (int i = 0; i < resps.length; i++) {
+            if (resps[i] == null) { // since MailboxInfo nulls it out when finishing parsing an identified response
+                continue;
+            }
+            final IMAPResponse ir = resps[i];
+
+            ir.skipSpaces();
+            if (ir.readByte() != '[') {
+                ir.reset();
+                continue;
+            }
+
+            String key = ir.readAtom();
+            if (key == null) { // no key present
+                ir.reset();
+                continue;
+            }
+            key = key.toUpperCase();
+            if (key.equals(MAILBOX_ID)) { // example when 26 is the mailbox id:"* OK [MAILBOXID (26)] Ok"
+                final String[] values = ir.readSimpleList(); // reading the string, aka as above example, "(26)", within parentheses
+                if (values != null && values.length >= 1) {
+                    mailboxId = Integer.valueOf(values[0]);
+                    resps[i] = null; // Nulls out this element in array to be consistent with MailboxInfo behavior
+                    break;
+                }
+            }
+            ir.reset(); // default back the parsing index
+        }
+    }
+
+    /**
+     * @return MAILBOXID, a server-allocated unique identifier for each mailbox. Please refer to OBJECTID, RFC 8474, for more detail.
+     */
+    @Nullable
+    public Integer getMailboxId() {
+        return mailboxId;
+    }
+}
---
+++ core/src/test/java/com/yahoo/imapnio/async/data/ExtensionMailboxInfoTest.java
@@ -0,0 +1,207 @@
+package com.yahoo.imapnio.async.data;
+
+import java.io.IOException;
+
+import javax.mail.Flags.Flag;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.sun.mail.iap.ProtocolException;
+import com.sun.mail.imap.protocol.IMAPResponse;
+import com.yahoo.imapnio.async.exception.ImapAsyncClientException;
+
+/**
+ * Unit test for {@code ExtensionMailboxInfo}.
+ */
+public class ExtensionMailboxInfoTest {
+
+    /**
+     * Tests calling constructor successfully.
+     *
+     * @throws IOException will not throw
+     * @throws ProtocolException will not throw
+     * @throws ImapAsyncClientException will not throw
+     */
+    @Test
+    public void testCreateExtensionMailboxInfoSuccess() throws IOException, ProtocolException, ImapAsyncClientException {
+
+        final IMAPResponse[] content = new IMAPResponse[9];
+        content[0] = new IMAPResponse("* 3 EXISTS");
+        content[1] = new IMAPResponse("* 0 RECENT");
+        content[2] = new IMAPResponse("* OK [UIDVALIDITY 1459808247] UIDs valid");
+        content[3] = new IMAPResponse("* OK [UIDNEXT 150400] Predicted next UID");
+        content[4] = new IMAPResponse("* FLAGS (\\Answered \\Deleted \\Draft \\Flagged \\Seen $Forwarded $Junk $NotJunk)");
+        content[5] = new IMAPResponse("* OK [PERMANENTFLAGS ()] No permanent flags permitted");
+        content[6] = new IMAPResponse("* OK [HIGHESTMODSEQ 614]");
+        content[7] = new IMAPResponse("* OK [MAILBOXID (2147483647)] Ok");
+        content[8] = new IMAPResponse("002 OK [READ-ONLY] EXAMINE completed; now in selected state");
+        final ExtensionMailboxInfo minfo = new ExtensionMailboxInfo(content);
+
+        // verify the result
+        Assert.assertNotNull(minfo, "result mismatched.");
+        Assert.assertNotNull(minfo.availableFlags, "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.ANSWERED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.DELETED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.DRAFT), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.FLAGGED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.SEEN), "availableFlags mismatched.");
+        Assert.assertEquals(minfo.highestmodseq, 614, "highestmodseq mismatched.");
+        Assert.assertEquals(minfo.uidvalidity, 1459808247, "uidvalidity mismatched.");
+        Assert.assertEquals(minfo.uidnext, 150400, "uidnext mismatched.");
+        Assert.assertEquals(minfo.getMailboxId(), new Integer(2147483647), "MailboxId mismatched.");
+        Assert.assertNull(content[7], "This element should be nulled out");
+    }
+
+    /**
+     * Tests calling constructor when there is other text code, like MAILBOXABC, expects getMailboxId() to return null.
+     *
+     * @throws IOException will not throw
+     * @throws ProtocolException will not throw
+     * @throws ImapAsyncClientException will not throw
+     */
+    @Test
+    public void testCreateExtensionMailboxInfoSomeOtherTextCode() throws IOException, ProtocolException, ImapAsyncClientException {
+
+        final IMAPResponse[] content = new IMAPResponse[9];
+        content[0] = new IMAPResponse("* 3 EXISTS");
+        content[1] = new IMAPResponse("* 0 RECENT");
+        content[2] = new IMAPResponse("* OK [UIDVALIDITY 1459808247] UIDs valid");
+        content[3] = new IMAPResponse("* OK [UIDNEXT 150400] Predicted next UID");
+        content[4] = new IMAPResponse("* FLAGS (\\Answered \\Deleted \\Draft \\Flagged \\Seen $Forwarded $Junk $NotJunk)");
+        content[5] = new IMAPResponse("* OK [PERMANENTFLAGS ()] No permanent flags permitted");
+        content[6] = new IMAPResponse("* OK [HIGHESTMODSEQ 614]");
+        content[7] = new IMAPResponse("* OK [MAILBOXABC (2147483647)] Ok");
+        content[8] = new IMAPResponse("002 OK [READ-ONLY] EXAMINE completed; now in selected state");
+        final ExtensionMailboxInfo minfo = new ExtensionMailboxInfo(content);
+
+        // verify the result
+        Assert.assertNotNull(minfo, "result mismatched.");
+        Assert.assertNotNull(minfo.availableFlags, "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.ANSWERED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.DELETED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.DRAFT), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.FLAGGED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.SEEN), "availableFlags mismatched.");
+        Assert.assertEquals(minfo.highestmodseq, 614, "highestmodseq mismatched.");
+        Assert.assertEquals(minfo.uidvalidity, 1459808247, "uidvalidity mismatched.");
+        Assert.assertEquals(minfo.uidnext, 150400, "uidnext mismatched.");
+        Assert.assertNull(minfo.getMailboxId(), "MailboxId mismatched, should not be set.");
+        Assert.assertNotNull(content[7], "This element should not be nulled out");
+        Assert.assertEquals(content[7].getRest(), "[MAILBOXABC (2147483647)] Ok", "The index is not reset to the point of the status code.");
+    }
+
+    /**
+     * Tests constructing an instance of ExtensionMailboxInfo when MAILBOX ID does not follow the text code format.
+     *
+     * @throws IOException will not throw
+     * @throws ProtocolException will not throw
+     * @throws ImapAsyncClientException will not throw
+     */
+    @Test
+    public void testCreateExtensionMailboxInfoLeftBracketNotFound() throws IOException, ProtocolException, ImapAsyncClientException {
+
+        final IMAPResponse[] content = new IMAPResponse[9];
+        content[0] = new IMAPResponse("* 3 EXISTS");
+        content[1] = new IMAPResponse("* 0 RECENT");
+        content[2] = new IMAPResponse("* OK [UIDVALIDITY 1459808247] UIDs valid");
+        content[3] = new IMAPResponse("* OK [UIDNEXT 150400] Predicted next UID");
+        content[4] = new IMAPResponse("* FLAGS (\\Answered \\Deleted \\Draft \\Flagged \\Seen $Forwarded $Junk $NotJunk)");
+        content[5] = new IMAPResponse("* OK [PERMANENTFLAGS ()] No permanent flags permitted");
+        content[6] = new IMAPResponse("* OK [HIGHESTMODSEQ 614]");
+        content[7] = new IMAPResponse("* OK MAILBOXID (2147483647) Ok");
+        content[8] = new IMAPResponse("002 OK [READ-ONLY] EXAMINE completed; now in selected state");
+        final ExtensionMailboxInfo minfo = new ExtensionMailboxInfo(content);
+
+        // verify the result
+        Assert.assertNotNull(minfo, "result mismatched.");
+        Assert.assertNotNull(minfo.availableFlags, "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.ANSWERED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.DELETED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.DRAFT), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.FLAGGED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.SEEN), "availableFlags mismatched.");
+        Assert.assertEquals(minfo.highestmodseq, 614, "highestmodseq mismatched.");
+        Assert.assertEquals(minfo.uidvalidity, 1459808247, "uidvalidity mismatched.");
+        Assert.assertEquals(minfo.uidnext, 150400, "uidnext mismatched.");
+        Assert.assertNull(minfo.getMailboxId(), "MailboxId mismatched since format not correct.");
+        Assert.assertNotNull(content[7], "This element should not be nulled out");
+        Assert.assertEquals(content[7].getRest(), "MAILBOXID (2147483647) Ok", "The index is not reset to the point of the status code.");
+    }
+
+    /**
+     * Tests constructing an instance of ExtensionMailboxInfo when MAILBOX ID does not follow the text code format.
+     *
+     * @throws IOException will not throw
+     * @throws ProtocolException will not throw
+     * @throws ImapAsyncClientException will not throw
+     */
+    @Test
+    public void testCreateExtensionMailboxInfoLeftParaenthesisNotFound() throws IOException, ProtocolException, ImapAsyncClientException {
+
+        final IMAPResponse[] content = new IMAPResponse[9];
+        content[0] = new IMAPResponse("* 3 EXISTS");
+        content[1] = new IMAPResponse("* 0 RECENT");
+        content[2] = new IMAPResponse("* OK [UIDVALIDITY 1459808247] UIDs valid");
+        content[3] = new IMAPResponse("* OK [UIDNEXT 150400] Predicted next UID");
+        content[4] = new IMAPResponse("* FLAGS (\\Answered \\Deleted \\Draft \\Flagged \\Seen $Forwarded $Junk $NotJunk)");
+        content[5] = new IMAPResponse("* OK [PERMANENTFLAGS ()] No permanent flags permitted");
+        content[6] = new IMAPResponse("* OK [HIGHESTMODSEQ 614]");
+        content[7] = new IMAPResponse("* OK [MAILBOXID 26] Ok");
+        content[8] = new IMAPResponse("002 OK [READ-ONLY] EXAMINE completed; now in selected state");
+        final ExtensionMailboxInfo minfo = new ExtensionMailboxInfo(content);
+
+        // verify the result
+        Assert.assertNotNull(minfo, "result mismatched.");
+        Assert.assertNotNull(minfo.availableFlags, "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.ANSWERED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.DELETED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.DRAFT), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.FLAGGED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.SEEN), "availableFlags mismatched.");
+        Assert.assertEquals(minfo.highestmodseq, 614, "highestmodseq mismatched.");
+        Assert.assertEquals(minfo.uidvalidity, 1459808247, "uidvalidity mismatched.");
+        Assert.assertEquals(minfo.uidnext, 150400, "uidnext mismatched.");
+        Assert.assertNull(minfo.getMailboxId(), "MailboxId mismatched since format not correct.");
+        Assert.assertNotNull(content[7], "This element should not be nulled out");
+        Assert.assertEquals(content[7].getRest(), "[MAILBOXID 26] Ok", "The index is not reset to the point of the status code.");
+    }
+
+    /**
+     * Tests constructing an instance of ExtensionMailboxInfo when MAILBOX ID does not follow the text code format.
+     *
+     * @throws IOException will not throw
+     * @throws ProtocolException will not throw
+     * @throws ImapAsyncClientException will not throw
+     */
+    @Test
+    public void testCreateExtensionMailboxInfoNoValueInParenthsis() throws IOException, ProtocolException, ImapAsyncClientException {
+
+        final IMAPResponse[] content = new IMAPResponse[9];
+        content[0] = new IMAPResponse("* 3 EXISTS");
+        content[1] = new IMAPResponse("* 0 RECENT");
+        content[2] = new IMAPResponse("* OK [UIDVALIDITY 1459808247] UIDs valid");
+        content[3] = new IMAPResponse("* OK [UIDNEXT 150400] Predicted next UID");
+        content[4] = new IMAPResponse("* FLAGS (\\Answered \\Deleted \\Draft \\Flagged \\Seen $Forwarded $Junk $NotJunk)");
+        content[5] = new IMAPResponse("* OK [PERMANENTFLAGS ()] No permanent flags permitted");
+        content[6] = new IMAPResponse("* OK [HIGHESTMODSEQ 614]");
+        content[7] = new IMAPResponse("* OK [MAILBOXID ()] Ok");
+        content[8] = new IMAPResponse("002 OK [READ-ONLY] EXAMINE completed; now in selected state");
+        final ExtensionMailboxInfo minfo = new ExtensionMailboxInfo(content);
+
+        // verify the result
+        Assert.assertNotNull(minfo, "result mismatched.");
+        Assert.assertNotNull(minfo.availableFlags, "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.ANSWERED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.DELETED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.DRAFT), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.FLAGGED), "availableFlags mismatched.");
+        Assert.assertTrue(minfo.availableFlags.contains(Flag.SEEN), "availableFlags mismatched.");
+        Assert.assertEquals(minfo.highestmodseq, 614, "highestmodseq mismatched.");
+        Assert.assertEquals(minfo.uidvalidity, 1459808247, "uidvalidity mismatched.");
+        Assert.assertEquals(minfo.uidnext, 150400, "uidnext mismatched.");
+        Assert.assertNull(minfo.getMailboxId(), "MailboxId mismatched since format not correct.");
+        Assert.assertNotNull(content[7], "This element should not be nulled out");
+        Assert.assertEquals(content[7].getRest(), "[MAILBOXID ()] Ok", "The index is not reset to the point of the status code.");
+    }
+}
