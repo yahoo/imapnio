80635b3c07b7ff825f09b24564e2f43bdfc5bce2
Fan Su <fansu.dev@gmail.com>
2021-03-17 14:10:03 -0700

    Migrate from bintray to Maven Central

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 if [ "${TRAVIS_BRANCH}" == 'master' ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ]; then
-    mvn deploy --settings ci/mvnsettings.xml
+    mkdir ci/deploy
+    openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/pubring.gpg.enc -out ci/deploy/pubring.gpg -d
+    openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/secring.gpg.enc -out ci/deploy/secring.gpg -d
+    gpg --fast-import ci/deploy/pubring.gpg
+    gpg --fast-import ci/deploy/secring.gpg
+
+    mvn deploy -P ossrh --settings ci/mvnsettings.xml
+    # delete decrypted keys
+    rm -rf ci/deploy
 fi
