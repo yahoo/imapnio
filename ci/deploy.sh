c10e9fabad3e38d988337218111cc7d071b231a4
Fan Su <fansu.dev@gmail.com>
2023-06-26 16:31:12 -0700

    temp

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-if [ "${TRAVIS_BRANCH}" == 'master' ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ]; then
-    mkdir ci/deploy
+mkdir ci/deploy
 
-    openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/pubkeys.asc.enc -out ci/deploy/pubkeys.asc -pbkdf2 -d
-    openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/prikeys.asc.enc -out ci/deploy/prikeys.asc -pbkdf2 -d
-    gpg --batch --fast-import ci/deploy/pubkeys.asc
-    gpg --batch --fast-import ci/deploy/prikeys.asc
+openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/pubkeys.asc.enc -out ci/deploy/pubkeys.asc -pbkdf2 -d
+openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/prikeys.asc.enc -out ci/deploy/prikeys.asc -pbkdf2 -d
+gpg --batch --fast-import ci/deploy/pubkeys.asc
+gpg --batch --fast-import ci/deploy/prikeys.asc
+ls ~/.gnupg/private-keys-v1.d/
 
-    mvn deploy -P ossrh --settings ci/mvnsettings.xml
-    # delete decrypted keys
-    rm -rf ci/deploy
-fi
+mvn deploy -P ossrh --settings ci/mvnsettings.xml
+# delete decrypted keys
+rm -rf ci/deploy
